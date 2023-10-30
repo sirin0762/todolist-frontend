69378f905c80ccc2273a0e607caf5cc130d5b55c
sirin0762 <tlsdydwls123@gmail.com>
2023-10-30 20:53:35 +0900

    FEAT: axios를 통해 mock 데이터 get

--- src/pages/DashBoard.vue
+++ src/pages/DashBoard.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="row">
     <div v-for="(task, i) in tasks" :key="i" class="col-4">
-      <div class="text-h5 text-weight-bold q-ma-xl">
+      <div class="text-h5 text-weight-bold q-ma-xl text-lowercase text-capitalize">
         <font-awesome-icon v-if="i === 0" icon="fa-d fa-cloud" style="color: #61b3e5" ></font-awesome-icon>
         <font-awesome-icon v-else-if="i === 1" icon="fa-solid fa-sun" style="color: #ff0000;" ></font-awesome-icon>
         <font-awesome-icon v-else-if="i === 2" icon="fa-solid fa-moon" style="color: #ffd43b;" ></font-awesome-icon>
-        {{task.name}}
+        {{capitalize(task.todoTimeOfDay.toLowerCase())}}
       </div>
       <draggable
-          :list="task.todos"
+          :list="task.todoResponses"
           group="people"
           :animation="200"
           item-key="id"
@@ -25,7 +25,25 @@
 import {useTodoListStore} from "../stores/todoList.js";
 import Todo from "../components/Todo.vue";
 import draggable from "vuedraggable";
+import {capitalize, computed, onMounted} from "vue";
+import axios from "axios";
+import {format} from "quasar";
 
 const todoListStore = useTodoListStore();
-const tasks = todoListStore.state.todoList;
+const {state} = todoListStore;
+let tasks = computed(() => state.todoList);
+
+onMounted(() => {
+  axios
+      .get("http://localhost:8080/api/todos", {
+        params: {
+          date: "2023-10-29"
+        }
+      })
+      .then((res) => {
+        todoListStore.state.todoList = res.data;
+        console.log(res.data);
+      })
+
+})
 </script>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -2,35 +2,7 @@ import {defineStore} from "pinia";
 
 export const useTodoListStore = defineStore("todoListStore", () => {
     const state = {
-        todoList: [
-            {
-                name: "Morning",
-                todos: [
-                    {id: 1, title: "Hello", desc: "world", done: false},
-                    {id: 2, title: "Hello", desc: "world", done: false},
-                    {id: 3, title: "Hello", desc: "world", done: false},
-                    {id: 4, title: "Hello", desc: "world", done: false},
-                ]
-            },
-            {
-                name: "Afternoon",
-                todos: [
-                    {id: 5, title: "Hello", desc: "world", done: false},
-                    {id: 6, title: "Hello", desc: "world", done: false},
-                    {id: 7, title: "Hello", desc: "world", done: false},
-                    {id: 8, title: "Hello", desc: "world", done: false},
-                ]
-            },
-            {
-                name: "Evening",
-                todos: [
-                    {id: 9, title: "Hello", desc: "world", done: false},
-                    {id: 10, title: "Hello", desc: "world", done: false},
-                    {id: 11, title: "Hello", desc: "world", done: false},
-                    {id: 12, title: "Hello", desc: "world", done: false},
-                ]
-            }
-        ]
+        todoList: []
     }
 
     return {state};
