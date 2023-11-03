32eb270f7f878704cd5072c823349e3e9d045e45
sirin0762 <tlsdydwls123@gmail.com>
2023-11-03 15:32:54 +0900

    FEAT: fixed sidebar로 변경

--- src/components/common/Drawer.vue
+++ src/components/common/Drawer.vue
@@ -1,63 +1,68 @@
 <template>
-  <q-drawer
-      v-model="drawer"
-      show-if-above
-      :width="256"
-      :breakpoint="400"
-      class="bg-indigo text-white"
-  >
-    <div style="height: 45%" class="q-pa-lg">
-      <div class="flex justify-center items-center column">
-        <q-avatar size="150px" class="q-mb-lg">
-          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-        </q-avatar>
-        <div class="text-weight-bold text-h5 q-mb-sm">Sirin</div>
-        <q-btn class="bg-white text-black">Update Profile</q-btn>
-      </div>
-    </div>
+  <div class="fixed">
+    <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="256"
+        :breakpoint="400"
+        class="bg-indigo text-white"
+    >
+      <q-scroll-area class="fit">
+        <div style="height: 45vh" class="q-pa-lg">
+          <div class="flex justify-center items-center column">
+            <q-avatar size="150px" class="q-mb-lg">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold text-h5 q-mb-sm">Sirin</div>
+            <q-btn class="bg-white text-black">Update Profile</q-btn>
+          </div>
+        </div>
 
-    <q-list padding>
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-icon name="inbox"/>
-        </q-item-section>
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="inbox"/>
+            </q-item-section>
 
-        <q-item-section>
-          Inbox
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              Inbox
+            </q-item-section>
+          </q-item>
 
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-icon name="star"/>
-        </q-item-section>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="star"/>
+            </q-item-section>
 
-        <q-item-section>
-          Star
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              Star
+            </q-item-section>
+          </q-item>
 
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-icon name="send"/>
-        </q-item-section>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="send"/>
+            </q-item-section>
 
-        <q-item-section>
-          Send
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              Send
+            </q-item-section>
+          </q-item>
 
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-icon name="drafts"/>
-        </q-item-section>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="drafts"/>
+            </q-item-section>
 
-        <q-item-section>
-          Drafts
-        </q-item-section>
-      </q-item>
-    </q-list>
-  </q-drawer>
+            <q-item-section>
+              Drafts
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+  </div>
+
 </template>
 
 <script setup>
