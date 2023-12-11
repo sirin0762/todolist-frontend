36103ee0350d798f891676b38ce3d058ccb8344d
sirin0762 <tlsdydwls123@gmail.com>
2023-12-11 15:47:47 +0900

    CHORE: CORS(5)

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,13 @@ dotenv.config()
 
 export default defineConfig({
     plugins: [vue()],
-    // server: {
-    //     proxy: {
-    //         "/api": {
-    //             target: process.env.VITE_SERVER_BASE_URL,
-    //             changeOrigin: true
-    //         }
-    //     }
-    // }
+    server: {
+        proxy: {
+            "/api": {
+                target: process.env.VITE_SERVER_BASE_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, "")
+            }
+        }
+    }
 })
