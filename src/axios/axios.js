7f069ea50dbec4bd9b3b1e0c82332305cefe5e33
sirin0762 <tlsdydwls123@gmail.com>
2023-12-11 16:37:28 +0900

    CHORE: CORS(10)

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const globalAxios = axios.create({
     baseURL: import.meta.env.VITE_SERVER_BASE_URL,
-    headers: {
-        "Access-Control-Allow-Origin": import.meta.env.VITE_BASE_URL,
-        "Access-Control-Allow-Credentials": true
-    }
+    // headers: {
+    //     "Access-Control-Allow-Origin": import.meta.env.VITE_BASE_URL,
+    //     "Access-Control-Allow-Credentials": true
+    // }
 });
 
 export default globalAxios
