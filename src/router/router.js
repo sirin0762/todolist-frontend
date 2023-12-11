a25196ae642ab190ac195a6512f6aa1648a33093
sirin0762 <tlsdydwls123@gmail.com>
2023-12-11 15:08:32 +0900

    CHORE:CORS(3)

--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.VITE_BASE_URL),
+    history: createWebHistory(),
     routes: routes,
     base: import.meta.env.VITE_BASE_URL
 });
