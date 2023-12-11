a4e739d703b09405dff0d95fc6478b47dc0027fb
sirin0762 <tlsdydwls123@gmail.com>
2023-12-11 16:10:24 +0900

    FEAT: CORS(8)

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const globalAxios = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_BASE_URL
+    baseURL: import.meta.env.VITE_SERVER_BASE_URL,
+    headers: {
+        "Access-Control-Allow-Origin": import.meta.env.VITE_BASE_URL,
+        "Access-Control-Allow-Credentials": true
+    }
 });
 
 export default globalAxios
